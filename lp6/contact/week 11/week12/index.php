3a03db2e0c15762a60de4560f60182a08c147c67
ApexDepth <114706809+ApexDepth@users.noreply.github.com>
2022-12-01 19:08:26 -0500

    Add files via upload

---
+++ lp6/contact/week 11/week12/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<title>
+ PHP Includes
+</title>
+<meta content="width=device-width, initial-scale=1" name="viewport">
+<meta charset="UTF-8">
+<meta content="This a page that will process a form." name="description">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1>PHP Includes</h1>
+    
+    <?php include "menu.php"; ?>
+    <?php include "footer.php"; ?>
+</body>
+</html>
---
+++ lp6/contact/week 11/week12/menu.php
@@ -0,0 +1,6 @@
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">Webography</a></li>
+<li><a href="#">Resume</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
